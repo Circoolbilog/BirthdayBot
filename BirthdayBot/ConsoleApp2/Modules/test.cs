96faec89c49e4f090d9add926987ba1c7c46c0bd
Circoolbilog <clarkllarena@ymail.com>
2017-09-17 11:44:49 +0800

    Add files via upload

---
+++ BirthdayBot/ConsoleApp2/Modules/test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace BirthdayBot.Modules
+{
+    public class test : ModuleBase<SocketCommandContext>
+    {
+        [Command("test")]
+        public async Task aosf()
+        {
+            await Context.Channel.SendMessageAsync("Bot Is Working Properly");
+        }
+    }
+}
---
+++ BirthdayBot/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        private CommandHandler _handler;
+
+        public async Task StartAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            new CommandHandler();
+
+            await _client.LoginAsync(TokenType.Bot, "MzU4NDE3MzEzMzExMDk2ODM0.DJ6hgQ.HVxAE_MJOKLhjsg1xF0wM6VsSog");
+
+            await _client.StartAsync();
+
+            _handler = new CommandHandler();
+
+            await _handler.InitializeAsync(_client);
+
+            await Task.Delay(-1);
+        }
+
+    }
+
+}
